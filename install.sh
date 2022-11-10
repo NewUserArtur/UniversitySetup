6d647468ea5d697ab84380c10b75a25301e5cfb4
Arthur Kravtsov <NewUserArtur>
2022-11-10 17:06:21 +0100

    added ampersands and superuser privileges for fonts

--- install.sh
+++ install.sh
@@ -20,29 +20,29 @@
 while true; do
     echo "Enter path where to install"
     {
-        read mypath
-        eval mypath=$mypath
-        mkdir -p $mypath
-        mkdir -p $mypath/.config
-        cp .config/sxhkdrc $mypath/.config
-        cp .config/tint2rc $mypath/.config
-        mydata=$(cat .config/sxhkdrc)
-        echo "${mydata//MYPATH/"$mypath"}" > $mypath/.config/sxhkdrc
-        cp -r .scripts $mypath
-        cp -r .templates $mypath
-        cp -r .UltiSnips $mypath
-        mkdir -p $HOME/.config/autostart
-        mydata=$(cat .config/sxhkd.desktop)
-        echo "${mydata//MYPATH/"$mypath"}" > $HOME/.config/autostart/sxhkd_uni.desktop
-        mkdir -p $HOME/.vim/autoload
-        cp .config/plug.vim $HOME/.vim/autoload
-        mydata=$(cat .config/.vimrc)
-        echo "${mydata//MYPATH/"$mypath"}" >> $HOME/.vimrc
-        mkdir -p /usr/share/vim/vim90/spell
-        cp .config/spell/* /usr/share/vim/vim90/spell
-        curl -LO https://github.com/SirVer/ultisnips/archive/master.zip
-        unzip master.zip
-        cp -r ultisnips $mypath/.config
+        read mypath &&
+        eval mypath=$mypath &&
+        mkdir -p $mypath &&
+        mkdir -p $mypath/.config &&
+        cp .config/sxhkdrc $mypath/.config &&
+        cp .config/tint2rc $mypath/.config &&
+        mydata=$(cat .config/sxhkdrc) &&
+        echo "${mydata//MYPATH/"$mypath"}" > $mypath/.config/sxhkdrc &&
+        cp -r .scripts $mypath &&
+        cp -r .templates $mypath &&
+        cp -r .UltiSnips $mypath &&
+        mkdir -p $HOME/.config/autostart &&
+        mydata=$(cat .config/sxhkd.desktop) &&
+        echo "${mydata//MYPATH/"$mypath"}" > $HOME/.config/autostart/sxhkd_uni.desktop &&
+        mkdir -p $HOME/.vim/autoload &&
+        cp .config/plug.vim $HOME/.vim/autoload &&
+        mydata=$(cat .config/.vimrc) &&
+        echo "${mydata//MYPATH/"$mypath"}" >> $HOME/.vimrc &&
+        mkdir -p /usr/share/vim/vim90/spell &&
+        sudo cp .config/spell/* /usr/share/vim/vim90/spell &&
+        curl -LO https://github.com/SirVer/ultisnips/archive/master.zip &&
+        unzip master.zip &&
+        cp -r ultisnips $mypath/.config &&
 
         break
     } || {
